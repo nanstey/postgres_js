007fb5cc73dd3e795bf2932f859b9f4fc7a7f552
Noel Anstey <anstey.ng@gmail.com>
2017-03-28 14:16:43 -0700

    clean up knex_people

--- knex_people.js
+++ knex_people.js
@@ -5,17 +5,10 @@ const knex = require('knex')({
   connection: settings
 })
 
-// knex.select().from('famous_people')
-//   .then( function(rows) {
-//     for (let i = 0; i < rows.length; i++){
-//       console.log(rows[i]);
-//     }
-//   });
-// console.log(results);
 
 let name = process.argv[2];
 getFamousPeopleByName(name);
-
+knex.destroy();
 
 function getFamousPeopleByName(name){
   console.log('Searching...')
@@ -34,31 +27,4 @@ function printResults(rows){
     console.log(`- ${row.id}: ${row.first_name} ${row.last_name}, born ${d}`);
   }
 }
-
-// getFamousPeopleByName(name, (result) => {
-//   let rows = result.rows.length;
-//   console.log(`Found ${rows} person(s) by the name '${name}':`)
-//   for (let i = 0; i < rows; i++){
-//     let row = result.rows[i];
-//     let d = new Date(row.birthdate).toDateString()
-//     console.log(`- ${row.id}: ${row.first_name} ${row.last_name}, born ${d}`);
-//   }
-// });
-
-
-// function getFamousPeopleByName(name, callback){
-//   client.connect((err) => {
-//     if (err) {
-//       return console.error("Connection Error", err);
-//     }
-//     console.log('Searching ...');
-//     client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text;", [name], (err, result) => {
-//       if (err) {
-//         return console.error("error running query", err);
-//       }
-//       callback(result);
-//       client.end();
-//     });
-//   });
-// }
 
